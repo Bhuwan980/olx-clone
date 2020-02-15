e358e5d25b2757e8dac29e3ebc558979f66e97f1
bhuwan neupane <bdr.bhuwan@gmail.com>
2020-02-15 13:11:15 +0545

    first commit

--- product/views.py
+++ product/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import DetailView,ListView
-from.models import Product
+from.models import Product,Category
 # Create your views here.
 class listview(ListView):
     template_name = 'product/listview.html'
-    model = Product
-    context_object_name = 'products'
+    model = Product 
+    context_object_name ='products'
+    paginate_by = 10
+    def get_context_data(self,**kwargs):
+        context = super(listview, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.all()
+        return context
+
 class detailview(DetailView):
     template_name = 'product/detailview.html'
     model = Product
     context_object_name = 'products'
+    # queryset = Product.objects.all()       
+
+    # def get_context_data(self,**kwargs):
+    #     context = super(detailview, self).get_context_data(**kwargs)
+    #     context['category'] = Category.objects.all()
+    #     return context
